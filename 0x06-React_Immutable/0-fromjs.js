330d9bfbcd4c5c3dd844887d1d5b436189d895a5
chessboy71 <bentegrimohamed@gmail.com>
2024-08-16 16:53:56 +0200

    nested.js

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -3,12 +3,3 @@ import { fromJS } from 'immutable';
 export default function getImmutableObject(obj) {
   return fromJS(obj);
 }
-
-console.log(
-  getImmutableObject({
-    fear: true,
-    smell: -1033575916.9145899,
-    wall: false,
-    thing: -914767132,
-  })
-);
--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -4,15 +4,3 @@ export default function accessImmutableObject(object, array) {
   const mpdObj = fromJS(object);
   return mpdObj.getIn(array, undefined);
 }
-
-console.log(
-  accessImmutableObject(
-    {
-      name: {
-        first: 'Guillaume',
-        last: 'Salva',
-      },
-    },
-    ['name', 'first']
-  )
-);
