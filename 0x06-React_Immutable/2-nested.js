85ac00d0022f72e3d83c08644fbdb1203e9da409
chessboy71 <bentegrimohamed@gmail.com>
2024-08-16 20:25:20 +0200

    merge

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,6 +1,7 @@
-import { fromJS, getIn } from './node_modules/immutable/dist/immutable';
+import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
   const mpdObj = fromJS(object);
+
   return mpdObj.getIn(array, undefined);
 }
