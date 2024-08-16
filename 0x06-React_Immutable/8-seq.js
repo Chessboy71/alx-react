3d56add37fe56729d3ab4a141ca5310566fbfe01
chessboy71 <bentegrimohamed@gmail.com>
2024-08-17 00:15:15 +0200

    seq

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,10 +1,11 @@
 import { Seq } from 'immutable';
 export default function printBestStudents(obj) {
   return Seq(obj)
-    .filter((x) => x.score < 70)
+    .filter((x) => x.score > 70)
     .map((x) => ({
       score: x.score,
       firstName: x.firstName.charAt(0).toUpperCase() + x.firstName.slice(1),
       lastName: x.lastName.charAt(0).toUpperCase() + x.lastName.slice(1),
-    }));
+    }))
+    .toJS();
 }
